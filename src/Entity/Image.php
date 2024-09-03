2b5040c9959ad726b916ec2d6269aa7438a704bb
youssouf doucoure <madkdouc@yahoo.com>
2024-09-03 19:46:07 +0200

    update:entity/image

---
+++ src/Entity/Image.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name_image = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $chemin_image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameImage(): ?string
+    {
+        return $this->name_image;
+    }
+
+    public function setNameImage(string $name_image): static
+    {
+        $this->name_image = $name_image;
+
+        return $this;
+    }
+
+    public function getCheminImage(): ?string
+    {
+        return $this->chemin_image;
+    }
+
+    public function setCheminImage(string $chemin_image): static
+    {
+        $this->chemin_image = $chemin_image;
+
+        return $this;
+    }
+}
